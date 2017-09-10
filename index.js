4dc5ecf90f01021c20b53c171157cd9652203e43
slam24 <luismartinez242011@gmail.com>
2017-09-10 14:59:34 -0600

    feat: Connect database

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
 	res.send('Hello world, this is graphql api.');
 });
 
+var models = require('./server/models/');
+models.sequelize
+  .authenticate()
+  .then(function () {
+    console.log('Connection successful');
+  })
+  .catch(function(error) {
+    console.log("Error creating connection:", error);
+  });
+
 app.listen(3000, () =>{
 	console.log('Server Running at port 3000');
 });
